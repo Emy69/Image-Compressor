4fdab86c9ca32fb2ebf85bbff05f1a2be3f26a92
Emy69 <viejone83@gmail.com>
2024-12-26 02:29:18 -0700

    refactor: ajuste en main para inicializar la nueva UI

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from gui.app import ImageOptimizerApp
 import customtkinter as ctk
 
 def main():
+    ctk.set_default_color_theme("blue")  # Puedes cambiar "blue" por "dark-blue" o "green"
     root = ctk.CTk()
     app = ImageOptimizerApp(root)
     root.mainloop()
